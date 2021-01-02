96f82abf786c9bdccaea251125875ed4d17f08b7
1712701 <haoquangms@gmail.com>
2021-01-02 09:00:30 +0700

    nt

--- main.cc
+++ main.cc
@@ -8,35 +8,75 @@
 
 vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
+    if(depth <= 0){
+        return vec3(0,0,0);
+    }
     if (world->hit(r, 0.001, MAXFLOAT, rec)) {
         ray scattered;
         vec3 attenuation;
-        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-            return attenuation*color(scattered, world, depth+1);
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation*color(scattered, world, depth - 1);
         }
-        else {
-            return vec3(0,0,0);
-        }
+        return vec3(0,0,0);  
     }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-    }
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    
 }
+hitable *random_scene() {
+    int n = 100;
+    int grid = (int)sqrt(n); 
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a =  - grid / 2; a <  grid /2; a++) {
+        for (int b = - grid / 2; b < grid / 2; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2,
+                        new lambertian(vec3(drand48()*drand48(),
+                                            drand48()*drand48(),
+                                            drand48()*drand48())
+                        )
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5*(1 + drand48()),
+                                           0.5*(1 + drand48()),
+                                           0.5*(1 + drand48())), 
+                                      0.5*drand48()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
 
+    // list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    // list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
 int main() {
-    int nx = 200;
-    int ny = 100;
-    int ns = 100;
+    int nx = 1000;
+    int ny = 500;
+    int ns = 10;
+    int max_depth = 50;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    hitable *list[4];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1.25,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8, 0.8, 0.8),0.1));
-    hitable *world = new hitable_list(list,4);
-    camera cam;
+    
+    hitable *world = random_scene();
+
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    camera cam(lookfrom, lookat, vup, 20, float(nx)/float(ny));
+
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
             vec3 col(0, 0, 0);
@@ -45,7 +85,7 @@ int main() {
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world,0);
+                col += color(r, world,max_depth);
             }
             col /= float(ns);
             col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
